f59a62428c6c9bacd48c1c502150cce25e9f5865
Berli04 <diego.berlanga@alumnos.upm.es>
2023-04-28 15:11:47 +0200

    Create PHONTANEROS.cpp

---
+++ PHONTANEROS.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	
+	//Declaracion de variables
+	int op;
+	
+	//Seleccionar programa
+	printf ("Introduzca una opcion: \n\n");
+	printf ("1 - Buscar lista de datos \n");
+	printf ("2 - Ordenar datos \n");
+	printf ("3 - ... \n");
+	printf ("4 - Salir del programa \n");
+	scanf ("%d", &op);
+	
+	
+	
+	return 0;
+}
