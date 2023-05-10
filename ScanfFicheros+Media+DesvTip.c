06c2ff9dc555dc5fe0fbd93785b65b0c6e850574
Davforgar <david.fortes@alumnos.upm.es>
2023-05-10 13:58:05 +0200

    desv
    desv

--- ScanfFicheros+Media+DesvTip.c
+++ ScanfFicheros+Media+DesvTip.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <math.h>
 
 struct TDatosFuente {
 	char fuente[500];
@@ -13,7 +14,12 @@ int main(){
 	float fmediapHLav(struct TDatosFuente[]);
 	float fmediapHVall(struct TDatosFuente[]);
 	
-	float pH, mediapHCar, mediapHLav, mediapHVall;
+	float fdespHCar(struct TDatosFuente[], float);
+	float fdespHLav(struct TDatosFuente[], float);
+	float fdespHVall(struct TDatosFuente[], float);
+	
+	
+	float pH, mediapHCar, mediapHLav, mediapHVall, despHCar, despHLav, despHVall;
 	int conductividad, turbidez, coliformes;
 	char fuente[500];
 	int i=0, n, opcion; 
@@ -92,18 +98,27 @@ int main(){
 	mediapHLav = fmediapHLav(FuenteLav);
 	mediapHVall = fmediapHVall(FuenteVall);
 	
+	despHCar = fdespHCar(FuenteCar, mediapHCar);
+	despHLav = fdespHLav(FuenteLav, mediapHLav);
+	despHVall = fdespHVall(FuenteCar, mediapHVall);
 	
 	printf("La de media de pH de Carabanchel es: %.2f\n", mediapHCar);
-	printf("La de media de pH de Carabanchel es: %.2f\n", mediapHLav);
-	printf("La de media de pH de Carabanchel es: %.2f\n", mediapHVall);
+	printf("La de media de pH de Lavapies es: %.2f\n", mediapHLav);
+	printf("La de media de pH de Vallecas es: %.2f\n", mediapHVall);
+	
+	printf("La de desviacion tipica de pH de Carabanchel es: %.2f\n", despHCar);
+	printf("La de desviacion tipica de pH de Lavapies es: %.2f\n", despHLav);
+	printf("La de desviacion tipica de pH de Vallecas es: %.2f\n", despHVall);
+	
+	return 0;
 }
 
 //Falta buscar la forma de meter el numero de fuentes de cada sitio en las funciones sin meter el numero, y asi solo hacer una funcion para media
 
 float fmediapHCar(struct TDatosFuente FuenteCar[]){
-	int i;
+	int i, nFuen = 30;
 	float mediapH = 0, mediapHCar;
-	for(i=0; i<30; i++){
+	for(i=0; i<nFuen; i++){
 		mediapH += FuenteCar[i].pH;
 	}
 	mediapHCar = mediapH / 30;
@@ -111,9 +126,9 @@ float fmediapHCar(struct TDatosFuente FuenteCar[]){
 }
 
 float fmediapHLav(struct TDatosFuente FuenteLav[]){
-	int i;
+	int i, nFuen = 25;
 	float mediapH = 0, mediapHLav;
-	for(i=0; i<25; i++){
+	for(i=0; i<nFuen; i++){
 		mediapH += FuenteLav[i].pH;
 	}
 	mediapHLav = mediapH / 25;
@@ -121,11 +136,41 @@ float fmediapHLav(struct TDatosFuente FuenteLav[]){
 }
 
 float fmediapHVall(struct TDatosFuente FuenteVall[]){
-	int i;
+	int i, nFuen = 27;
 	float mediapH = 0, mediapHVall;
-	for(i=0; i<27; i++){
+	for(i=0; i<nFuen; i++){
 		mediapH += FuenteVall[i].pH;
 	}
 	mediapHVall = mediapH / 27;
 	return mediapHVall;
+}
+
+float fdespHCar(struct TDatosFuente FuenteCar[], float mediapHCar){
+	int i, nFuen = 30;
+	float sumdespH = 0, despHCar=0;
+	for(i=0; i<nFuen; i++){
+		sumdespH += (FuenteCar[i].pH - mediapHCar)*(FuenteCar[i].pH - mediapHCar);
+	}
+	despHCar = sqrt(sumdespH/nFuen);
+	return despHCar;
+}
+
+float fdespHLav(struct TDatosFuente FuenteLav[], float mediapHLav){
+	int i, nFuen = 25;
+	float sumdespH = 0, despHLav=0;
+	for(i=0; i<nFuen; i++){
+		sumdespH += (FuenteLav[i].pH - mediapHLav)*(FuenteLav[i].pH - mediapHLav);
+	}
+	despHLav = sqrt(sumdespH/nFuen);
+	return despHLav;
+}
+
+float fdespHVall(struct TDatosFuente FuenteVall[], float mediapHVall){
+	int i, nFuen = 27;
+	float sumdespH = 0, despHVall=0;
+	for(i=0; i<nFuen; i++){
+		sumdespH += (FuenteVall[i].pH - mediapHVall)*(FuenteVall[i].pH - mediapHVall);
+	}
+	despHVall = sqrt(sumdespH/nFuen);
+	return despHVall;
 }
