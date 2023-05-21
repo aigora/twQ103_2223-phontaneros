3845fbcb9b9476ef2b94c2dc8a6351aa464d48ba
Davforgar <david.fortes@alumnos.upm.es>
2023-05-21 18:52:02 +0200

    Update PH_ScanfFicheros+FunModa.c

--- PH_ScanfFicheros+FunModa.c
+++ PH_ScanfFicheros+FunModa.c
@@ -8,9 +8,23 @@ struct TDatosFuente {
 	int coliformes;
 };
 //Prototipos de las funciones
-float fmodaLav(struct TDatosFuente[]);
-float fmodaCar(struct TDatosFuente[]);
-float fmodaVall(struct TDatosFuente[]);
+float fmodaLavpH(struct TDatosFuente[], int);
+float fmodaCarpH(struct TDatosFuente[], int);
+float fmodaVallpH(struct TDatosFuente[], int);
+
+float fmodaCarCon(struct TDatosFuente[], int);
+float fmodaLavCon(struct TDatosFuente[], int);
+float fmodaVallCon(struct TDatosFuente[], int);
+
+float fmodaCarTur(struct TDatosFuente[], int);
+float fmodaLavTur(struct TDatosFuente[], int);
+float fmodaVallTur(struct TDatosFuente[], int);
+
+float fmodaCarCol(struct TDatosFuente[], int);
+float fmodaLavCol(struct TDatosFuente[], int);
+float fmodaVallCol(struct TDatosFuente[], int);
+
+
 
 int main () {
 
@@ -23,7 +37,7 @@ int main () {
 	int conductividad, turbidez, coliformes;
 	char fuente[500];
 	int opcion;
-	float valor_repetidoCar, valor_repetidoLav, valor_repetidoVall;
+	float valor_repetidopH, valor_repetidoCon, valor_repetidoTur, valor_repetidoCol;
 	struct TDatosFuente FuenteLav[500];
 	struct TDatosFuente FuenteCar[500];
 	struct TDatosFuente FuenteVall[500];
@@ -53,8 +67,7 @@ int main () {
 	fclose (fCarabanchel);
 	NumFuentesCar = i; 
 	
-	valor_repetidoCar = fmodaCar(FuenteCar);
-	printf("El pH mas repetido de Carabanchel %.2f \n", valor_repetidoCar);
+	
 	
 	//Lavapies
 	FILE *fLavapies;
@@ -77,8 +90,6 @@ int main () {
 	fclose (fLavapies);
 	NumFuentesLav = i;
 	
-	valor_repetidoLav = fmodaLav(FuenteLav);
-	printf("El pH mas repetido de Lavapies es %.2f \n", valor_repetidoLav);
 	
 	//Vallecas
 	FILE *fVallecas;
@@ -102,9 +113,43 @@ int main () {
 	fclose (fVallecas);
 	NumFuentesVall = i;
 	
-	valor_repetidoVall = fmodaVall(FuenteVall);
-	printf("El pH mas repetido de Vallecas es %.2f \n", valor_repetidoVall);	
-		
+	valor_repetidopH = fmodaCarpH(FuenteCar, NumFuentesCar);
+	printf("El pH mas repetido de Carabanchel %.2f \n", valor_repetidopH);
+	
+	valor_repetidopH = fmodaLavpH(FuenteLav, NumFuentesLav);
+	printf("El pH mas repetido de Lavapies es %.2f \n", valor_repetidopH);
+	
+	valor_repetidopH = fmodaVallpH(FuenteVall, NumFuentesVall);
+	printf("El pH mas repetido de Vallecas es %.2f \n", valor_repetidopH);	
+	
+	valor_repetidoCon = fmodaCarCon(FuenteCar, NumFuentesCar);
+	printf("El valor de conductividad mas repetido de Carabanchel es %.2f \n", valor_repetidoCon);
+	
+	valor_repetidoCon = fmodaLavCon(FuenteLav, NumFuentesLav);
+	printf("El valor de conductividad mas repetido de Lavapies es %.2f \n", valor_repetidoCon);
+	
+	valor_repetidoCon = fmodaVallCon(FuenteVall, NumFuentesVall);
+	printf("El valor de conductividad mas repetido de Vallecas es %.2f \n", valor_repetidoCon);
+	
+	valor_repetidoTur = fmodaCarTur(FuenteCar, NumFuentesCar);
+	printf("El valor de turbidez mas repetido de Carabanchel es %.2f \n", valor_repetidoTur);
+	
+	valor_repetidoTur = fmodaLavTur(FuenteLav, NumFuentesLav);
+	printf("El valor de turbidez mas repetido de Lavapies es %.2f \n", valor_repetidoTur);
+	
+	valor_repetidoTur = fmodaVallTur(FuenteVall, NumFuentesVall);
+	printf("El valor de turbidez mas repetido de Vallecas es %.2f \n", valor_repetidoTur);
+	
+	valor_repetidoCol = fmodaCarCol(FuenteCar, NumFuentesCar);
+	printf("El valor de coliformes mas repetido de Carabanchel es %.2f \n", valor_repetidoCol);
+	
+	valor_repetidoCol = fmodaLavCol(FuenteLav, NumFuentesLav);
+	printf("El valor de coliformes mas repetido de Lavapies es %.2f \n", valor_repetidoCol);
+	
+	valor_repetidoCol = fmodaVallCol(FuenteVall, NumFuentesVall);
+	printf("El valor de coliformes mas repetido de Vallecas es %.2f \n", valor_repetidoCol);		
+	
+	
 	//Cerrar ficheros
 	fclose (fLavapies);
 	fclose (fCarabanchel);
@@ -113,76 +158,260 @@ int main () {
 	return 0;
 }
 
-//Carabanchel
+//pH
+	//Carabanchel
+	
+	float fmodaCarpH(struct TDatosFuente FuenteCar[], int NumFuentesCar) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux1;
+	
+	    for (i = 0; i < NumFuentesCar; i++) {
+	        for (j = 0; j < NumFuentesCar; j++) {
+	            if (FuenteCar[i].pH == FuenteCar[j].pH && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux1 = FuenteCar[i].pH;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux1;  
+	}
+	
+	//Lavapies
+	
+	float fmodaLavpH(struct TDatosFuente FuenteLav[], int NumFuentesLav) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux2;
+	
+	    for (i = 0; i < NumFuentesLav; i++) {
+	        for (j = 0; j < NumFuentesLav; j++) {
+	            if (FuenteLav[i].pH == FuenteLav[j].pH && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux2 = FuenteLav[i].pH;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux2;  
+	}
+	
+	//Vallecas
+	
+	float fmodaVallpH(struct TDatosFuente FuenteVall[], int NumFuentesVall) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesVall; i++) {
+	        for (j = 0; j < NumFuentesVall; j++) {
+	            if (FuenteVall[i].pH == FuenteVall[j].pH && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteVall[i].pH;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
 
-float fmodaCar(struct TDatosFuente FuenteCar[]) {
-    int i, j, cont2 = 0, cont1 = 0;
-    float aux1;
+//Conductividad
+	//Carabanchel
+	float fmodaCarCon(struct TDatosFuente FuenteCar[], int NumFuentesCar) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesCar; i++) {
+	        for (j = 0; j < NumFuentesCar; j++) {
+	            if (FuenteCar[i].conductividad == FuenteCar[j].conductividad && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteCar[i].conductividad;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	float fmodaLavCon(struct TDatosFuente FuenteLav[], int NumFuentesLav) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesLav; i++) {
+	        for (j = 0; j < NumFuentesLav; j++) {
+	            if (FuenteLav[i].conductividad == FuenteLav[j].conductividad && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteLav[i].conductividad;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	//Vallecas
+	float fmodaVallCon(struct TDatosFuente FuenteVall[], int NumFuentesVall) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesVall; i++) {
+	        for (j = 0; j < NumFuentesVall; j++) {
+	            if (FuenteVall[i].conductividad == FuenteVall[j].conductividad && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteVall[i].conductividad;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
 
-    for (i = 0; i < 30; i++) {
-        for (j = 0; j < 30; j++) {
-            if (FuenteCar[i].pH == FuenteCar[j].pH && i != j) {
-                cont1++;
-            }
-        }
-
-        if (cont1 > cont2) {
-            cont2 = cont1;
-            aux1 = FuenteCar[i].pH;
-        }
-
-        cont1 = 0;
-    }
-
-    return aux1;  
-}
-
-//Lavapies
-
-float fmodaLav(struct TDatosFuente FuenteLav[]) {
-    int i, j, cont2 = 0, cont1 = 0;
-    float aux2;
-
-    for (i = 0; i < 25; i++) {
-        for (j = 0; j < 25; j++) {
-            if (FuenteLav[i].pH == FuenteLav[j].pH && i != j) {
-                cont1++;
-            }
-        }
-
-        if (cont1 > cont2) {
-            cont2 = cont1;
-            aux2 = FuenteLav[i].pH;
-        }
-
-        cont1 = 0;
-    }
-
-    return aux2;  
-}
-
-//Vallecas
-
-float fmodaVall(struct TDatosFuente FuenteVall[]) {
-    int i, j, cont2 = 0, cont1 = 0;
-    float aux3;
-
-    for (i = 0; i < 30; i++) {
-        for (j = 0; j < 30; j++) {
-            if (FuenteVall[i].pH == FuenteVall[j].pH && i != j) {
-                cont1++;
-            }
-        }
-
-        if (cont1 > cont2) {
-            cont2 = cont1;
-            aux3 = FuenteVall[i].pH;
-        }
-
-        cont1 = 0;
-    }
-
-    return aux3;  
-}
-
-
+//Turbidez
+	//Carabanchel
+	float fmodaCarTur(struct TDatosFuente FuenteCar[], int NumFuentesCar) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesCar; i++) {
+	        for (j = 0; j < NumFuentesCar; j++) {
+	            if (FuenteCar[i].turbidez == FuenteCar[j].turbidez && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteCar[i].turbidez;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	//Lavapies
+	float fmodaLavTur(struct TDatosFuente FuenteLav[], int NumFuentesLav) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesLav; i++) {
+	        for (j = 0; j < NumFuentesLav; j++) {
+	            if (FuenteLav[i].turbidez == FuenteLav[j].turbidez && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteLav[i].turbidez;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	//Vallecas
+	float fmodaVallTur(struct TDatosFuente FuenteVall[], int NumFuentesVall) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesVall; i++) {
+	        for (j = 0; j < NumFuentesVall; j++) {
+	            if (FuenteVall[i].turbidez == FuenteVall[j].turbidez && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteVall[i].turbidez;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+//Coliformes
+	//Carabanchel
+	float fmodaCarCol(struct TDatosFuente FuenteCar[], int NumFuentesCar) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesCar; i++) {
+	        for (j = 0; j < NumFuentesCar; j++) {
+	            if (FuenteCar[i].coliformes == FuenteCar[j].coliformes && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteCar[i].coliformes;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	//Lavapies
+	float fmodaLavCol(struct TDatosFuente FuenteLav[], int NumFuentesLav) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesLav; i++) {
+	        for (j = 0; j < NumFuentesLav; j++) {
+	            if (FuenteLav[i].coliformes == FuenteLav[j].coliformes && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteLav[i].coliformes;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
+	
+	//Vallecas
+	float fmodaVallCol(struct TDatosFuente FuenteVall[], int NumFuentesVall) {
+	    int i, j, cont2 = 0, cont1 = 0;
+	    float aux3;
+	
+	    for (i = 0; i < NumFuentesVall; i++) {
+	        for (j = 0; j < NumFuentesVall; j++) {
+	            if (FuenteVall[i].coliformes == FuenteVall[j].coliformes && i != j) {
+	                cont1++;
+	            }
+	        }
+	
+	        if (cont1 > cont2) {
+	            cont2 = cont1;
+	            aux3 = FuenteVall[i].coliformes;
+	        }
+	        cont1 = 0;
+	    }
+	    return aux3;  
+	}
